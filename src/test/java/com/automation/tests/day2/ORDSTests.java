b65c66caf57c685624c66d0d499444ca3f957cb3
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-05-17 14:01:56 -0400

    commit

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,4 +1,33 @@
 package com.automation.tests.day2;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
 public class ORDSTests {
+    String BASE_URL = "http://3.90.112.152:1000/ords/hr";
+
+    @Test
+    @DisplayName("Get list of all employees")
+    public void getAllEmployees() {
+        //response can be saved in the Response object
+        //prettyPeek() - method that prints response info in nice format
+        //also ths method returns Response object
+        //response contains body, header and status line
+        //body (payload) - contains content that we requested from the web service
+        //header - contains meta data
+        Response response = given().baseUri(BASE_URL).when().get("/employees").prettyPeek();
+
+        /**
+         * RestAssured request has similar structure to BDD scenarios:
+         * Start building the request part of the test
+         *
+         * given() - used for request setup and authentication
+         * Syntactic sugar,
+         * when() - to specify type of HTTP request: get, put, post, delete, patch, head, etc...
+         * then() - to verify response, perform assertions
+         */
+    }
 }
